a1ccc0cdbd128639b9eea8eb5b1dac1679a8ff85
BoatGT-9 <twz192545@gmail.com>
2023-04-10 14:13:22 +0700

    first commit

---
+++ Test Suites/TS1001.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c856ab4f-57e8-41e9-908d-70f6349e15ec</testSuiteGuid>
+</TestSuiteEntity>
